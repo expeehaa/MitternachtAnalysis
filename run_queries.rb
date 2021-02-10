7c912a62a827c1f5223f3542a3a829a5a57ac085
expeehaa <expeehaa@outlook.com>
2021-02-10 21:18:05 +0100

    Indent empty lines, fix spelling.

--- run_queries.rb
+++ run_queries.rb
@@ -37,12 +37,12 @@ with_db do |db|
 	query db, 'data/dailymoney.csv', <<~Q
 		SELECT subject.*, unh."Name" FROM (SELECT count(*) AS times_received_dailymoney, subject."UserId" FROM "DailyMoneyStats" AS subject WHERE #{GUILD_CONDITION} GROUP BY subject."UserId") AS subject #{NAME_JOIN_QUERY} ORDER BY subject.times_received_dailymoney DESC
 	Q
-
+	
 	query db, 'data/dailymoney_sum.csv', <<~Q
 		SELECT subject.*, unh."Name" FROM (SELECT sum(subject."MoneyReceived") AS dailymoney_received, subject."UserId" FROM "DailyMoneyStats" AS subject WHERE #{GUILD_CONDITION} GROUP BY subject."UserId") AS subject #{NAME_JOIN_QUERY} ORDER BY subject.dailymoney_received DESC
 	Q
-
-	query db, 'data/total_daily_money.csv', <<~Q
+	
+	query db, 'data/total_dailymoney.csv', <<~Q
 		SELECT sum(subject."MoneyReceived") AS total_dailymoney_received, count(*) AS total_times_dailymoney_collected FROM "DailyMoneyStats" AS subject WHERE #{GUILD_CONDITION}
 	Q
 	
@@ -53,15 +53,15 @@ with_db do |db|
 	query db, 'data/voicestats.csv', <<~Q
 		SELECT subject."TimeInVoiceChannel", subject."UserId", unh."Name" FROM "VoiceChannelStats" AS subject #{NAME_JOIN_QUERY} WHERE #{GUILD_CONDITION} ORDER BY subject."TimeInVoiceChannel" DESC
 	Q
-
+	
 	query db, 'data/nickname_changes.csv', <<~Q
 		SELECT subject.*, unh."Name" FROM (SELECT count(*) AS amount_nickname_changes, subject."UserId" FROM "UsernameHistory" AS subject WHERE subject."Discriminator" = 'NicknameHistoryModel' AND #{GUILD_CONDITION} GROUP BY subject."UserId") AS subject #{NAME_JOIN_QUERY} ORDER BY subject.amount_nickname_changes DESC
 	Q
-
+	
 	query db, 'data/dailymoney_per_week.csv', <<~Q
 		SELECT to_char(date_trunc('week', "DateAdded"), 'YYYY-MM-DD') AS time, count(*) AS collected_dailymoney FROM "DailyMoneyStats" AS subject WHERE #{GUILD_CONDITION} GROUP BY time ORDER BY time
 	Q
-
+	
 	query db, 'data/dailymoney_per_month.csv', <<~Q
 		SELECT to_char(date_trunc('month', "DateAdded"), 'YYYY-MM') AS time, count(*) AS collected_dailymoney FROM "DailyMoneyStats" AS subject WHERE #{GUILD_CONDITION} GROUP BY time ORDER BY time
 	Q
